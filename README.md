451cfe5a67f437657a5e77ab18729f644d5f4a71
imuguruza <inigo.muguruza89@gmail.com>
2018-08-30 17:03:14 +0200

    First commit: add flash script and update README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nuttx_scripts
 Nuttx Scripts for making easier dev's life
+
+Place this folder at same level as `nuttx` and `apps` location
---
+++ flash.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase nuttx/nuttx.bin 0x08000000"
+
