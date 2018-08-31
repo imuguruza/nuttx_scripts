b8fd9d6ee3aea527d7d6640d44fc78f1ab6265cc
imuguruza <inigo.muguruza89@gmail.com>
2018-08-31 09:47:18 +0200

    Add usage help

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # nuttx_scripts
 Nuttx Scripts for making easier dev's life
 
-Place this folder at same level as `nuttx` and `apps` location
+`flas.sh`: Place the script at same level as `nuttx` and `apps` location
 
 ### Usage
 
 Supports Olimex STM32 E407 and STM32L Discovery boards, depending your target board, use:
 
-`./flash olimex`
-or
-`./flash stm32l1`
+`./flash olimex` or `./flash stm32l1`
--- flash.sh
+++ flash.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 
-if [ $1 = "olimex" ]; then
-	openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase ../nuttx/nuttx.bin 0x08000000"
+if [ -z "$1" ]; then
+	echo "usage: flas.sh olimex|stm32l1"
+
+elif [ $1 = "olimex" ]; then
+	openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase nuttx/nuttx.bin 0x08000000"
 
 elif [ $1 = "stm32l1"]; then
-	openocd -f interface/stlink-v2.cfg -f target/stm32l1.cfg -c init -c "reset halt" -c "flash write_image erase ../nuttx/nuttx.bin 0x08000000"
+	openocd -f interface/stlink-v2.cfg -f target/stm32l1.cfg -c init -c "reset halt" -c "flash write_image erase nuttx/nuttx.bin 0x08000000"
 
 else
-	echo "type olimex or stm32l1 to flash your target board" >&2
+	echo "usage: flash.sh olimex|stm32l1" >&2
 fi
